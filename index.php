5a0f77fd577fa3f03bdb6734ae77ffe97e1458ee
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-03-18 14:52:47 +0000

    First webpage PHP

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--Allows the Use of Favicons-->
+      <link rel="apple-touch-icon" sizes="180x180" href="./faviconphp1/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="./faviconphp1/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="./faviconphp1/favicon-16x16.png">
+      <link rel="manifest" href="./faviconphp1/site.webmanifest">
 		<title>First Webpage in PHP</title>
 	</head>
 	<body>
